38de8831bc4de27a337988de99ee56d81ea0d4d2
Ryan D'souza <dsouzarc@gmail.com>
2015-02-07 19:01:43 -0500

    Successfully added pinch to zoom

--- Hypnosister/AppDelegate.m
+++ Hypnosister/AppDelegate.m
@@ -9,73 +9,79 @@
 #import "AppDelegate.h"
 #import "BNRHypnosisView.h"
 
-@interface AppDelegate ()
+@interface AppDelegate () <UIScrollViewDelegate>
+
+@property (nonatomic) BNRHypnosisView *normalBNR;
 
 @end
 
 @implementation AppDelegate
 
-
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     
-    /*CGRect frame1 = self.window.bounds; //CGRectMake(160, 240, 100, 150);
-    
-    BNRHypnosisView *view1 = [[BNRHypnosisView alloc] initWithFrame:frame1];
-    //view1.backgroundColor = UIColor.redColor;
-    [self.window addSubview:view1]; */
-    
-    //CGRects for frames
+    // Create CGRects for frames
     CGRect screenRect = self.window.bounds;
     CGRect bigRect = screenRect;
-    bigRect.size.width *= 2;
-    //bigRect.size.height *= 2;
+    bigRect.size.width *= 2.0;
     
-    //Screensized scrollview
+    // Create a screen-sized scroll view, set zoom properties, and add it to the window
     UIScrollView *scrollView = [[UIScrollView alloc] initWithFrame:screenRect];
+    scrollView.maximumZoomScale = 2.0;
     [self.window addSubview:scrollView];
     
-    //Forces scroll so you focus on only one view at a time
-    scrollView.pagingEnabled = true; //Can also be 'YES'
+    // Create a screen-sized hypnosis view and add it to the scroll view
+    self.normalBNR = [[BNRHypnosisView alloc] initWithFrame:screenRect];
+    [scrollView addSubview:self.normalBNR];
     
-    //Normal Hypnosis view and add it to the scroll view
-    BNRHypnosisView *normalBNR = [[BNRHypnosisView alloc] initWithFrame:screenRect];
-    [scrollView addSubview:normalBNR];
+    // Removed view below to implement pinch-to-zoom in original hypnosis view (silver challenge in chapter 7)
+    // Create a second screen-sized hypnosis view and add it off the screen to the right
+    //screenRect.origin.x += screenRect.size.width;
+    //BNRHypnosisView *anotherView = [[BNRHypnosisView alloc] initWithFrame:screenRect];
+    //[scrollView addSubview:anotherView];
     
-    //Tell it how big its child is
+    // Tell the scroll view how big its content area is
     scrollView.contentSize = bigRect.size;
     
-    //Off center everything
-    screenRect.origin.x += screenRect.size.width;
-    BNRHypnosisView *offCenterBNR = [[BNRHypnosisView alloc] initWithFrame:screenRect];
-    [scrollView addSubview:offCenterBNR];
+    // Set the scroll view's delegate
+    scrollView.delegate = self;
     
-    
-    self.window.backgroundColor = UIColor.whiteColor;
-    self.window.makeKeyAndVisible;
+    self.window.backgroundColor = [UIColor whiteColor];
+    [self.window makeKeyAndVisible];
     return YES;
 }
 
-- (void)applicationWillResignActive:(UIApplication *)application {
+- (void)applicationWillResignActive:(UIApplication *)application
+{
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
 }
 
-- (void)applicationDidEnterBackground:(UIApplication *)application {
+- (void)applicationDidEnterBackground:(UIApplication *)application
+{
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
 }
 
-- (void)applicationWillEnterForeground:(UIApplication *)application {
+- (void)applicationWillEnterForeground:(UIApplication *)application
+{
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
 }
 
-- (void)applicationDidBecomeActive:(UIApplication *)application {
+- (void)applicationDidBecomeActive:(UIApplication *)application
+{
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
 }
 
-- (void)applicationWillTerminate:(UIApplication *)application {
+- (void)applicationWillTerminate:(UIApplication *)application
+{
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+}
+
+- (UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView
+{
+    return self.normalBNR;
 }
 
 @end
